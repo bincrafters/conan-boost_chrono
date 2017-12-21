9c28e38c0a7912a513b098bbc2b1c34f48f56fdd
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:31 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,34 +3,34 @@ from conans import ConanFile
 
 class BoostChronoConan(ConanFile):
     name = "Boost.Chrono"
-    version = "1.65.1"
+    version = "1.66.0"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Integer/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Predef/1.65.1@bincrafters/testing", \
-        "Boost.Ratio/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.System/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Typeof/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing", \
-        "Boost.Winapi/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Integer/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Predef/1.66.0@bincrafters/testing", \
+        "Boost.Ratio/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.System/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Typeof/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing", \
+        "Boost.Winapi/1.66.0@bincrafters/testing"
     lib_short_names = ["chrono"]
     is_header_only = False
 
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-chrono"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     generators = "boost"
     settings = "os", "arch", "compiler", "build_type"
