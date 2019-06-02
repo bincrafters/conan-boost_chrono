64f61589a92a062f956f3ce7dd6c21362552df77
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:24 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,29 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostChronoConan(base.BoostBaseConan):
     name = "boost_chrono"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_chrono"
-    lib_short_names = ["chrono"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_integer",
-        "boost_move",
-        "boost_mpl",
-        "boost_predef",
-        "boost_ratio",
-        "boost_static_assert",
-        "boost_system",
-        "boost_throw_exception",
-        "boost_type_traits",
-        "boost_typeof",
-        "boost_utility",
-        "boost_winapi"
-    ]
+    version = "1.70.0"
